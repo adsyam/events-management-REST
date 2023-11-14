3b3246c5556d55bfd6fb5c5e111fdb873caeb1a0
adsyam <yambaoadrianne@gmail.com>
2023-11-14 14:52:24 +0800

    AttendeeController + api paginate

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendee extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id'];
 
     public function user(): BelongsTo
     {
