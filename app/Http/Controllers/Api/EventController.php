5f6d7ba944cc1ac1f3ab217d24d066e834a423fe
adsyam <yambaoadrianne@gmail.com>
2023-11-14 11:08:58 +0800

    Update & destroy

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,38 +13,67 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return Event::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
+    /**
+     * Store a new event.
+     *
+     * @param Request $request The HTTP request object.
+     * @return Event The created event object.
+     */
     public function store(Request $request)
     {
-        //
+        // Create a new event object using the validated request data
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        // Return the created event object
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ]),
+        );
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response(status: 204);
     }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,11 +11,16 @@ class Event extends Model
 {
     use HasFactory;
 
+    // The attributes that are mass assignable.
+    protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
+
+    // Get the user associated with the event.
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    // Get the attendees associated with the event.
     public function attendees(): HasMany
     {
         return $this->hasMany(Attendee::class);
