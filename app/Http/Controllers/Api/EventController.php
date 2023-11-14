b332b0b6eddcfe569bb66efc674b5dd226be3225
adsyam <yambaoadrianne@gmail.com>
2023-11-14 11:30:30 +0800

    resources + user & attendee association

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -39,7 +40,7 @@ class EventController extends Controller
         ]);
 
         // Return the created event object
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -47,7 +48,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -64,7 +66,7 @@ class EventController extends Controller
             ]),
         );
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
